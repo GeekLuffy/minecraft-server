2531faa32349f30f18c94e40c2451db03e753617
GeekLuffy <71143694+GeekLuffy@users.noreply.github.com>
2025-04-03 15:01:51 +0530

    Update and rename start.sh to entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Starting Minecraft Server..."
+java -Xms4G -Xmx7G -jar paper.jar nogui
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Starting Minecraft Server..."
-java -Xms4G -Xmx7G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -jar paper.jar nogui
